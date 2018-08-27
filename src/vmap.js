42f3cc537adad72886526c06dfea2a2e51581f97
Victor JOUIN <victor.jouin@gmail.com>
2018-08-27 10:59:31 +0200

    Fix vmap condition

--- src/vmap.js
+++ src/vmap.js
@@ -3,10 +3,7 @@ import { childrenByName, parseXMLNode } from './parser_utils';
 
 class VMAP {
   constructor(xml) {
-    if (
-      (xml !== null ? xml.documentElement : undefined) === null ||
-      xml.documentElement.localName !== 'VMAP'
-    ) {
+    if (!xml || !xml.documentElement || !xml.documentElement.localName !== 'VMAP') {
       throw new Error('Not a VMAP document');
     }
 
